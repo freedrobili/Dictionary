335615536ae0a07fe05a9372faacae01544f8946
Yuliya Kudashkina <kudashkina-yuliya@mail.ru>
2023-09-07 12:43:07 +0300

    v1.3

--- src/main/java/com/example/Dictionary/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/Dictionary/service/impl/CategoryServiceImpl.java
@@ -12,7 +12,6 @@ import ru.tinkoff.eacq.mma.dictionary.rest.model.CategoriesResponse;
 import ru.tinkoff.eacq.mma.dictionary.rest.model.ShortCategories;
 import ru.tinkoff.eacq.mma.dictionary.rest.model.ShortCategoriesResponse;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -35,7 +34,7 @@ public class CategoryServiceImpl implements CategoryService {
         }
         ShortCategories shortCategories = new ShortCategories();
         List<ShortCategoriesResponse> shortCategoriesResponses = categories.stream()
-                .map(this::buildToShortCategories)
+                .map(this::buildShortCategories)
                 .collect(Collectors.toList());
 
         shortCategories.setCategories(shortCategoriesResponses);
@@ -48,10 +47,16 @@ public class CategoryServiceImpl implements CategoryService {
         Specification<Category> spec = buildSpecification(code, name, viewToClients, viewToManagers, needsDocs, product, risky);
 
         List<Category> categoryList = categoryRepository.findAll(spec);
-        return convertToCategoriesResponse(categoryList);
+        Categories categories = new Categories();
+        List<CategoriesResponse> categoriesResponses = categoryList.stream()
+                .map(this::buildCategoriesResponse)
+                .collect(Collectors.toList());
+
+        categories.setCategories(categoriesResponses);
+        return categories;
     }
 
-    private ShortCategoriesResponse buildToShortCategories(Category category) {
+    private ShortCategoriesResponse buildShortCategories(Category category) {
         ShortCategoriesResponse response = new ShortCategoriesResponse();
         response.setCode(category.getCategoryCode());
         response.setName(category.getProduct());
@@ -61,62 +66,35 @@ public class CategoryServiceImpl implements CategoryService {
         return response;
     }
 
+    private CategoriesResponse buildCategoriesResponse(Category category) {
+        CategoriesResponse response = new CategoriesResponse();
+        response.setCode(category.getCategoryCode());
+        response.setName(category.getCategoryTab().getName());
+        response.setNeedsDocs(category.getNeedsDocs());
+        response.setNeedsDocsReason(category.getNeedsDocsReason());
+        response.setNeedsDocsList(category.getNeedsDocsList());
+        response.setViewToClients(category.getViewToClients());
+        response.setViewToManagers(category.getViewToManagers());
+        response.setProduct(category.getProduct());
+        response.setRisky(category.getRisky());
+
+        return response;
+    }
+
     private Specification<Category> buildSpecification(String code, String name, Boolean viewToClients, Boolean viewToManagers, Boolean needsDocs, String product, Boolean risky) {
         Specification<Category> spec = Specification.where(null);
 
-        if (code != null) {
-            spec = spec.and(CategorySpecifications.hasCategoryCode(code));
-        }
-
-        if (name != null) {
-            spec = spec.and(CategorySpecifications.hasCategoryTabName(name));
-        }
-
-        if (viewToClients != null) {
-            spec = spec.and(CategorySpecifications.hasViewToClients(viewToClients));
-        }
-
-        if (viewToManagers != null) {
-            spec = spec.and(CategorySpecifications.hasViewToManagers(viewToManagers));
-        }
-
-        if (needsDocs != null) {
-            spec = spec.and(CategorySpecifications.hasNeedsDocs(needsDocs));
-        }
-
-        if (product != null) {
-            spec = spec.and(CategorySpecifications.hasProduct(product));
-        }
-
-        if (risky != null) {
-            spec = spec.and(CategorySpecifications.hasRisky(risky));
-        }
+        spec = spec.and(CategorySpecifications.isEqual("categpryCode", code))
+                .and(CategorySpecifications.isEqual("categoryTab.name", name))
+                .and(CategorySpecifications.isEqual("viewToClients", viewToClients))
+                .and(CategorySpecifications.isEqual("viewToManagers", viewToManagers))
+                .and(CategorySpecifications.isEqual("needsDocs", needsDocs))
+                .and(CategorySpecifications.isEqual("product", product))
+                .and(CategorySpecifications.isEqual("risky", risky));
 
         return spec;
     }
-
-    private Categories convertToCategoriesResponse(List<Category> categoryList) {
-        Categories categories = new Categories();
-        List<CategoriesResponse> categoriesResponses = new ArrayList<>();
-
-        for (Category category : categoryList) {
-            CategoriesResponse response = new CategoriesResponse();
-            response.setCode(category.getCategoryCode());
-            response.setName(category.getCategoryTab().getName());
-            response.setNeedsDocs(category.getNeedsDocs());
-            response.setNeedsDocsReason(category.getNeedsDocsReason());
-            response.setNeedsDocsList(category.getNeedsDocsList());
-            response.setViewToClients(category.getViewToClients());
-            response.setViewToManagers(category.getViewToManagers());
-            response.setProduct(category.getProduct());
-            response.setRisky(category.getRisky());
-
-            categoriesResponses.add(response);
-        }
-
-        categories.setCategories(categoriesResponses);
-        return categories;
-    }
 }
+
 
 
--- src/main/java/com/example/Dictionary/specifications/CategorySpecifications.java
+++ src/main/java/com/example/Dictionary/specifications/CategorySpecifications.java
@@ -4,48 +4,13 @@ import com.example.Dictionary.model.Category;
 import org.springframework.data.jpa.domain.Specification;
 
 public class CategorySpecifications {
-    public static Specification<Category> hasCategoryTabName(String name) {
-        return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.join("categoryTab").get("name"), name);
-    }
-    public static Specification<Category> hasCategoryCode(String categoryCode) {
-        return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("categoryCode"), categoryCode);
-    }
-
-    public static Specification<Category> hasProduct(String product) {
-        return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("product"), product);
-    }
-
-    public static Specification<Category> hasViewToClients(Boolean viewToClients) {
-        return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("viewToClients"), viewToClients);
-    }
-
-    public static Specification<Category> hasViewToManagers(Boolean viewToManagers) {
-        return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("viewToManagers"), viewToManagers);
-    }
-
-    public static Specification<Category> hasNeedsDocs(Boolean needsDocs) {
-        return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("needsDocs"), needsDocs);
-    }
-
-    public static Specification<Category> hasNeedsDocsReason(String needsDocsReason) {
-        return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("needsDocsReason"), needsDocsReason);
-    }
-
-    public static Specification<Category> hasNeedsDocsList(String needsDocsList) {
-        return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("needsDocsList"), needsDocsList);
-    }
-
-    public static Specification<Category> hasRisky(Boolean risky) {
-        return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("risky"), risky);
+    public static Specification<Category> isEqual(String key, Object value){
+        return (root, query, criteriaBuilder)->{
+            if (value == null){
+                return criteriaBuilder.isTrue(criteriaBuilder.literal(true));
+            }
+            return  criteriaBuilder.equal(root.get(key), value);
+        };
     }
 }
 
